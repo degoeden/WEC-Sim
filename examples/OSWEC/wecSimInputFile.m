cdd60560c533d391a356f34e269f1ce0bf7a3025
degoeden <degoeden8@gmail.com>
2026-09-09 09:34:55 -0400

    adjusted placement of FoH

--- examples/OSWEC/wecSimInputFile.m
+++ examples/OSWEC/wecSimInputFile.m
@@ -6,7 +6,7 @@ simu.explorer = 'on';                   % Turn SimMechanics Explorer (on/off)
 simu.startTime = 0;                     % Simulation Start Time [s]
 simu.rampTime = 100;                    % Wave Ramp Time [s]
 simu.endTime = 400;                     % Simulation End Time [s]        
-simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
+simu.solver = 'daessc';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
 simu.dt = 0.1;                          % Simulation Time-Step [s]
 simu.cicEndTime = 30;                   % Specify CI Time [s]
 
